cd1eda6104ffe0062b7b9eae453de5d5813d252b
Oliver Christensen <oliverchristensen93@gmail.com>
2019-11-14 14:38:17 +0100

    Test af modelklasser

--- test/testEmployee.js
+++ test/testEmployee.js
@@ -1,9 +1,9 @@
 const controller = require('../Controllers/controller');
 let expect = require('chai').expect
-    , firsttry = controller.createEmployee("Anders", "test@test.dk", "")
+    , firsttry = controller.createEmployee("0123456789","Anders", "test@test.dk", "")
     , secondtry = controller.createEmployee("test")
     , thirdtry = controller.createEmployee()
-    , fourthtry = controller.createEmployee("123456789", "Anders", "test@test.dk", "test");
+    , fourthtry = controller.createEmployee("0123456789", "Anders", "test@test.dk", "test");
 
 describe('unitTest', () => {
     it('make an employee with one parameter as an empty string', () => {
@@ -21,7 +21,7 @@ describe('unitTest', () => {
     it('make an employee with normal parameters', () => {
         expect(fourthtry.name).to.equal("Anders");
         expect(fourthtry.email).to.equal("test@test.dk");
-        expect(fourthtry.CPR).to.equal("123456789");
+        expect(fourthtry.CPR).to.equal("0123456789");
         expect(fourthtry.phoneNo).to.equal("test");
         expect(fourthtry.shifts.length).to.equal(0);
     });
--- test/testShift.js
+++ test/testShift.js
@@ -1,29 +1,40 @@
 const controller = require('../Controllers/controller');
+
+let testStartDate = new Date(2019, 11, 15,10,25);
+let testEndDate = new Date(2019, 11, 15,18,25);
+
 let expect = require('chai').expect
-    , firsttry = controller.createShift(Date, "test@test.dk", "")
-    , secondtry = controller.createShift("test")
-    , thirdtry = controller.createShift()
-    , fourthtry = controller.createShift("123456789", "Anders", "test@test.dk", "test");
+    , firsttry = controller.createShift(testStartDate)
+    , secondtry = controller.createShift()
+    , thirdtry = controller.createShift(testStartDate, testEndDate)
+    , fourthtry = controller.createShift(testEndDate, testStartDate);
 
 describe('unitTest', () => {
-    it('make an employee with one parameter as an empty string', () => {
+    it('Make a shift with only one date', () => {
         expect(firsttry).to.equal(undefined);
     });
 
-    it('make an employee with only one parameters', () => {
+    it('make a shift with no parameters', () => {
         expect(secondtry).to.equal(undefined);
     });
 
-    it('make an employee with no parameters', () => {
-        expect(thirdtry).to.equal(undefined);
+    it('make a shift with normal parameters', () => {
+        expect(thirdtry.start.getFullYear()).to.equal(2019);
+        expect(thirdtry.start.getMonth).to.equal(11);
+        expect(thirdtry.start.getDate()).to.equal(15);
+        expect(thirdtry.start.getHours()).to.equal(10);
+        expect(thirdtry.start.getMinutes()).to.equal(25);
+        expect(thirdtry.end.getFullYear()).to.equal(2019);
+        expect(thirdtry.end.getMonth).to.equal(11);
+        expect(thirdtry.end.getDate()).to.equal(15);
+        expect(thirdtry.end.getHours()).to.equal(18);
+        expect(thirdtry.end.getMinutes()).to.equal(25);
+        expect(thirdtry.totalHours).to.equal(480);
+        expect(thirdtry.employee).to.equal(undefined);
     });
 
-    it('make an employee with normal parameters', () => {
-        expect(fourthtry.name).to.equal("Anders");
-        expect(fourthtry.email).to.equal("test@test.dk");
-        expect(fourthtry.CPR).to.equal("123456789");
-        expect(fourthtry.phoneNo).to.equal("test");
-        expect(fourthtry.shifts.length).to.equal(0);
+    it('Make a shift with end date before startdate', () => {
+        expect(secondtry).to.equal(undefined);
     });
 
 });
