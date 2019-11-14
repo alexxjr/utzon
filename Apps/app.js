445d5ad3c1ce494a15c7bac84069f8f632867dce
Oliver Wisborg <wisborg95@live.dk>
2019-11-14 09:06:29 +0100

    Start up commit

---
+++ Apps/app.js
@@ -0,0 +1,11 @@
+// INITIALIZATION
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+app.use(express.json());
+
+// START THE SERVER
+const port = process.env.PORT || 9119;
+app.listen(port);
+console.log('Listening on port ' + port + ' ...');
