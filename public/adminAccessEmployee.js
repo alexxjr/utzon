d7c0152967192740c9d937f19d3d048a7d568782
Oliver Wisborg <wisborg95@live.dk>
2019-12-03 10:30:37 +0100

    variables

--- public/adminAccessEmployee.js
+++ public/adminAccessEmployee.js
@@ -2,7 +2,7 @@ let employeeSelectAdminAccessEmployee = document.querySelector("#select2");
 
 function modalAction() {
     if (userRole === "Admin" || userRole === "Employee") {
-        document.querySelector(".dropdown-content").style.visibility ="hidden";
+        dropdown_content.style.visibility ="hidden";
         document.getElementById("empModal").style.display = "block";
         document.getElementById("select2").value = "";
         document.getElementById("fromDatePicker").value = "0000-00-00";
@@ -13,7 +13,7 @@ function modalAction() {
 
 function closeModalAction() {
     document.getElementById("empModal").style.display = "none";
-    document.querySelector(".dropdown-content").style.visibility ="visible";
+    dropdown_content.style.visibility ="visible";
 }
 
 async function totalHoursBetweenTwoDates() {
